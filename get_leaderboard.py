7752abfc144a5767453af6168956163ed23229b9
Logan Lessen <loganlessen@gmail.com>
2024-09-30 22:03:21 -0600

    Move progress into live

--- get_leaderboard.py
+++ get_leaderboard.py
@@ -1,21 +1,16 @@
 import requests
 import argparse
-import schedule
 import time
 import os
-from typing import Union
 from rich import print as rprint
 from rich.progress import Progress
 from rich.table import Table
 from rich.layout import Layout
+from rich.live import Live
 
 
 def main():
-    def get_leaderboard():
-        os.system("clear")
-        user_ranking = {}
-        output = ["\n"]
-        errors = ["\n"]
+    def get_leaderboard(user_ranking, errors):
         with Progress(transient=True) as progress:
             leaderboard = progress.add_task("[cyan]Getting leaderboard...", total=len(usernames))
             for username in usernames: # Gets the ranking for each username from codewars
@@ -27,20 +22,24 @@ def main():
                         errors.append(f"[red]{username} could not be found")
                 progress.update(leaderboard, advance=1)
         layout = Layout()
+        layout.split_column(Layout(name="upper"))
         if args.display:
             layout.split_column(
                 Layout(name="upper"),
                 Layout(name="lower")
             )
             layout["lower"].split_row(
-                Layout(name="left"),
-                Layout(name="right"),
+                *(Layout(name=group) for group in groups_list)
             )
-            layout["lower"].split_row(
-                Layout(name="center"),
-                Layout(name="other_center"),
-            )
-        rprint(layout)
+            for group in groups_list:
+                table = Table(title=group, show_edge=False, show_lines=True, expand=True)
+                table.add_column("Rank", ratio=3, style="blue")
+                table.add_column("User", ratio=20, style="green")
+                table.add_column("Honor", ratio=6, style="magenta", justify="right")
+                group_rank = {user:user_ranking[user] for user in groups if groups[user] == group and (user in user_ranking)}
+                for rank, user in enumerate(reversed(sorted(group_rank, key=group_rank.get))):
+                    table.add_row(str(rank + 1), user, str(user_ranking[user]))
+                layout["lower"][group].update(table)
         table = Table(title="Leaderboard", show_edge=False, show_lines=True, expand=True)
         table.add_column("Rank", ratio=3, style="blue")
         table.add_column("User", ratio=20, style="green")
@@ -48,20 +47,19 @@ def main():
         table.add_column("Honor", ratio=6, style="magenta", justify="right")
         for rank, user in enumerate(reversed(sorted(user_ranking, key=user_ranking.get))):
             table.add_row(str(rank + 1), user, groups.get(user, ""), str(user_ranking[user]))
-        output.append(table)
+        layout["upper"].update(table)
         if not args.surpress:
-            output.extend(errors)
-        for message in output:
-            rprint(message)
+            rprint(errors)
+        return layout
 
 
     parser = argparse.ArgumentParser(description="Takes a list of usernames and returns the codewars leaderboard with those users")
     parser.add_argument("input", nargs="*", help="Usernames or files of usernames. If the input is a filepath, the program will use ach line in the file as a username. If it is not a valid file path, the program will interpret it as a username. To force the program to interpret your input as a username or file, use -u or -f respectuively")
     parser.add_argument("-f", "--file", action="append", help="The program will use this as a file path to get usernames from")
     parser.add_argument("-u", "--username", action="append", help="The program will use this as a username to rank")
-    parser.add_argument("-t", "--time", type=int,  help="Sets the program to run every x minutes")
+    parser.add_argument("-t", "--time", type=int, default=0, help="Sets the program to run every x minutes")
     parser.add_argument("-s", "--surpress", action="store_true", help="Supresses any error messages from missing usernames. Note: Does not supress any file-related errors")
-    parser.add_argument("-g", "--group", nargs="+", action="append", help="Add a group of users. The first argument is the name of the group, and anything after that is either files or usernames")
+    parser.add_argument("-g", "--group", nargs="+", action="append", default=[], help="Add a group of users. The first argument is the name of the group, and anything after that is either files or usernames")
     parser.add_argument("-d", "--display", action="store_true", help="Displays individual group leaderboards in addition to the overall leaderboard")
     args = parser.parse_args()
 
@@ -102,13 +100,13 @@ def main():
                 groups[user] = group_name
                 groups_list[group_name].append(username)
 
-    if args.time:
-            schedule.every(args.time).minutes.do(get_leaderboard)
-            while True:
-                schedule.run_pending()
-                time.sleep(1)
-    
-    get_leaderboard()
+    with Live(get_leaderboard(), screen=True, transient=True) as live:      
+        while True:
+            live.update(get_leaderboard())
+            time.sleep(args.time * 60)
+            if not args.time:
+                live.stop()
+
 
 def parse_file(path):
     try:
